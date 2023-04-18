e13706cfcf511f63c859cf3ff656f908d1770a2f
Avesh Singh <aveshzoop@gmail.com>
2023-04-18 23:20:20 +0530

    initial commit

---
+++ src/main/java/com/mycompany/mp3player/Mp3Player.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mp3player;
+
+/**
+ *
+ * @author avesh
+ */
+public class Mp3Player {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
